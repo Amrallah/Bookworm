b3247a3c45930bf62fc7a3768aca0f0ed521b001
Abdelrahman <amrallah97@gmail.com>
2019-05-04 17:16:47 +0200

    Update arabicdetector.py

--- arabicdetector.py
+++ arabicdetector.py
@@ -1,20 +1,27 @@
-
-
 import cv2
 import numpy as np
 import math
-from skimage.io import imread
-from skimage.color import rgb2gray
-from scipy import signal as sig
 import pytesseract
 from PIL import Image
 import glob
 import os
+import re
 
-img = cv2.imread('aa.png')
-os.makedirs("aa")
-print( pytesseract.image_to_string(Image.open('aa.png'),lang='ara'))
 
+def removeSpecial(text):
+    result = re.sub(r"[\'\.,@?$%_»«:،٬٫٭؟؛٪ـ]", "", text, flags=re.I)
+    return result
+
+def removeHamza(test):
+    length=len(test)
+    while (True):
+        if test[length-1]=="ء":
+            test = test[0:length-2]
+            length=len(test)
+            print(length)
+        else:
+            break
+    return test
 
 def prepareImg(img, height):
 	"""convert given image to grayscale image (if needed) and resize to desired height"""
@@ -49,25 +56,30 @@ def createKernel(kernelSize, sigma, theta):
     return kernel
 
 
-kernel1 = np.ones((3,3), np.uint8)
-# apply filter kernel
+dot_kernel = np.ones((1,1),np.uint8)
 
+circle_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
+bigger_circle_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+bigger_circle_kernel[0][1]=1
+bigger_circle_kernel[0][3]=1
+bigger_circle_kernel[4][1]=1
+bigger_circle_kernel[4][3]=1
+
+img = cv2.imread('01.png')
 img = prepareImg(img,600)
-img1 =img
-cv2.imshow('i',img)
-img = cv2.erode(img, kernel1, iterations=2)
-print(kernel1)
-img = cv2.dilate(img, kernel1, iterations=1)
-print(kernel1)
-cv2.imshow('im77',img)
-kernel = createKernel(kernelSize=21, sigma=11, theta=7)
+
+if not os.path.exists('cropped'):
+    os.makedirs('cropped')
+
+img1 = img
+
+kernel = createKernel(kernelSize=15, sigma=11, theta=6)
 imgFiltered = cv2.filter2D(img, -1, kernel, borderType=cv2.BORDER_REPLICATE).astype(np.uint8)
-cv2.imshow('im4',img)
+imgFiltered=cv2.dilate(imgFiltered,circle_kernel,iterations=5)
 (_, imgThres) = cv2.threshold(imgFiltered, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 imgThres = 255 - imgThres
-#
-cv2.imshow('imd',imgThres)
+cv2.imshow("Binary",imgThres)
 
 # Find the contours
 contours,hierarchy = cv2.findContours(imgThres,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
@@ -85,48 +97,42 @@ for c in contours:
 
 
 res= sorted(res, key=lambda entry:entry[0][0])
-
 i=0
-
-
 w, h = 5,len(res)
 Matrix = [[0 for x in range(w)] for y in range(h)]
 print('Segmented into %d words' % len(res))
+
 for (j, w) in enumerate(res):
     (wordBox, wordImg) = w
     (x, y, w, h) = wordBox
-    #cv2.rectangle(img1, (x, y), (x + w, y + h), 0, 1)
-    crop_img = img1[y-1:y + h+1, x-1:x + w+1]
-    Matrix[i][0] = y
-    Matrix[i][1] =y+h
-    Matrix[i][2] =x
-    Matrix[i][3] = x+w
-    Matrix[i][4] = 'aa/pic000%d.png' % (i,)
-    #img.save('temp2.png')
-    #cv2.imshow('pic{:>05}.jpg'.format(i), crop_img)
-    cv2.imwrite('aa/pic{:>05}.png'.format(i), crop_img)
+    yr=range(y-10,y+h+10)
+    xr=range(x-5,x+w+2)
+    for k in range(img1.shape[0]):
+         for g in range(img1.shape[1]):
+            if not( g  in xr and k in yr):
+                img1[k, g] = 255
+    Matrix[i][0] = y-10
+    Matrix[i][1] =y+h+10
+    Matrix[i][2] =x-5
+    Matrix[i][3] = x+w+2
+    Matrix[i][4] = 'cropped\pic000%d.png' % (i,)
+    cv2.imwrite('cropped/pic{:>05}.png'.format(i), img1)
+    img = cv2.imread('01.png')
+    img1 = prepareImg(img, 600)
     i += 1
-    print(x)
 
-images = glob.glob("aa/*.jpg")
-
-
-# j=0
-# for image in images:
-#     arabic01 = cv2.imread(image, 1)
-#     d=arabic_processing(arabic01, False)
-#     cv2.imwrite('dd/pic{:>05}.jpg'.format(j), d)
-#     j +=1
 k=0
-arr=[]
-y="يشير"
-yarab = glob.glob("aa/*.png")
-for image in yarab  :
-    with open(image, 'rb') as file:
+y="اللوح»"
 
-     text = pytesseract.image_to_string(Image.open(file),lang='ara',config='--psm 10')
+croppedImages = glob.glob("cropped/*.png")
+for image in croppedImages:
+    with open(image, 'rb') as file:
+     text = pytesseract.image_to_string(Image.open(file),lang='ara',config=" --psm 10 ")
+     text=removeSpecial(text)
+     y=removeSpecial(y)
+     text=removeHamza(text)
+     y=removeHamza(y)
      if text == y:
-         # for j in range(len(res)):
          for t in range(len(res)):
              if (str(image) == str(Matrix[t][4])):
                  cv2.rectangle(img1, (Matrix[t][2], Matrix[t][0]), (Matrix[t][3], Matrix[t][1]), 0, 1)
@@ -136,20 +142,5 @@ for image in yarab  :
      k +=1
 
 cv2.imshow('im',img1)
-#
-# # for cnt in contours:
-# #     x,y,w,h = cv2.boundingRect(cnt)
-# #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-# #     cv2.rectangle(thresh_color,(x,y),(x+w,y+h),(0,255,0),2)
-# #     crop_img = img[y:y + h, x:x + w]
-# #     #cv2.imwrite('pic{:>05}.jpg'.format(i), crop_img)
-# #     #cv2.imshow('pic{:>05}.jpg'.format(i), crop_img)
-# #     i+=1
-#
-# #cv2.imshow('img',img)
-# #cv2.imshow('res',thresh_color)
-#
+
 cv2.waitKey(0)
-
-
-
